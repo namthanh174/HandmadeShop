383ff90c05389031a5b332c1d941a2a4d1c7d309
namth <namth@DESKTOP-QGPJMF8>
2016-06-03 14:57:35 +0700

    Add domain entities for project

---
+++ HandMadeShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,36 @@
+﻿using HandMadeShop.Model.Abstract;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandMadeShop.Model.Models
+{
+    [Table("ProductCategories")]
+    public class ProductCategory : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [MaxLength(256)]
+        [Required]
+        public string Name { set; get; }
+
+        [MaxLength(256)]
+        [Required]
+        [Column(TypeName ="varchar")]
+        public string Alias { set; get; }
+
+        [MaxLength(500)]
+        public string Description { set; get; }
+        public int? ParentID { set; get; }
+        public int? DisplayOrder { set; get; }
+
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        public bool? HotFlag { set; get; }
+
+        public virtual IEnumerable<Product> Products { set; get; }
+    }
+}
